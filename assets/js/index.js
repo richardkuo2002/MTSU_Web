3b79e2dda12feb6611695518433ec575c2e6bc9f
Eevee <richardkuo2002@gmail.com>
2023-03-31 15:34:05 +0800

    wrapper

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+$(function(){
+	var dir=-1;
+	var interval=3000;
+	var duration=700;
+	var timer;
+	$("#slide ul").prepend($("#slide li:last-child"));
+	$("#slide ul").css("left" ,-1000);
+	timer=setInterval(slideTimer, interval);
+	function slideTimer(){
+		if (dir==-1) {
+			$("#slide ul").animate({"left":"-=1000px"},duration,function(){
+			$(this).append($("#slide li:first-child"));
+			$(this).css("left",-1000);
+		});
+		}else{
+			$("#slide ul").animate({"left":"+=1000px"},duration,function(){
+			$(this).prepend($("#slide li:last-child"));
+			$(this).css("left",-1000);
+			dir = -1;
+			});
+		}
+	}
+
+    $("#prevBtn").click(function(){
+        dir = 1;
+        clearInterval(timer);
+        timer = setInterval(slideTimer, interval);
+        slideTimer();
+    });
+    
+    $("#nextBtn").click(function(){
+        dir = -1;
+        clearInterval(timer);
+        timer = setInterval(slideTimer, interval);
+        slideTimer();
+    });
+});
