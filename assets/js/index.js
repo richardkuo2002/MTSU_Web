685ab81c71231f35b5b9e2544358af6a584b3e11
Eevee <richardkuo2002@gmail.com>
2023-03-31 18:26:22 +0800

    jquery

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,13 @@
 $(function(){
-	var dir=-1;
-	var interval=3000;
-	var duration=700;
+	var dir = -1;
+	var interval = 3000;
+	var duration = 700;
 	var timer;
 	$("#slide ul").prepend($("#slide li:last-child"));
 	$("#slide ul").css("left" ,-1000);
-	timer=setInterval(slideTimer, interval);
+	timer = setInterval(slideTimer, interval);
 	function slideTimer(){
-		if (dir==-1) {
+		if (dir == -1) {
 			$("#slide ul").animate({"left":"-=1000px"},duration,function(){
 			$(this).append($("#slide li:first-child"));
 			$(this).css("left",-1000);
