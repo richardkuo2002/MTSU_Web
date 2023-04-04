eaca986509981a8a4192e047bb99864342622242
Eevee <richardkuo2002@gmail.com>
2023-04-04 14:50:01 +0800

    jquery

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,10 +2,10 @@ $(function(){
 	$(window).scroll(function(){
 		if($(window).scrollTop()>10){
 			$("header").addClass("small");
-
+			$(".main").addClass("scroll");
 		}else{
-
 			$("header").removeClass("small");
+            $(".main").removeClass("scroll");
 		}
 	});
 });
